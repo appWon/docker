cc5e3129d4cf2ebd247ab2af762fa2e4224b41b7
장재원 <jangjaewon@jangjaewon-ui-MacBookPro.local>
2021-08-16 10:53:50 +0900

    settings: eslint, prettier

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  plugins: ['react', 'prettier'],
+  parser: 'babel-eslint',
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    semi: 'error',
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+  },
+};
---
+++ frontend/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return <div>초기세팅</div>;
+};
+
+ReactDom.render(<App />, document.getElementById('root'));
