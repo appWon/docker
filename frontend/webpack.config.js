5e20b5aa9bd9cd99891972c2125c8cdcd8fbbb20
장재원 <jangjaewon@jangjaewon-ui-MacBookPro.local>
2021-08-16 11:25:33 +0900

    setting: webpack babel

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,1 +1,31 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname + '/public/dist/'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    port: 3000,
+  },
+};
